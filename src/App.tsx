885bd6acbed8ea61fd6f377601bce6a58669c543
Sachin KM <hiscan99@gmail.com>
2024-06-17 16:43:59 +0200

    set up home page and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import HomeCards from "./components/HomeCards.jsx";
-import JobListings from "./components/JobListings.jsx";
-import ViewAllJobs from "./components/ViewAllJobs.jsx";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;

e1ea75aaa67e551a699e6bf17cf004841207b7dc
Sachin KM <hiscan99@gmail.com>
2024-06-18 14:00:15 +0200

    single job page using useEffect()

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -22,7 +22,6 @@ const JobListings = ({ isHome = false }) => {
     };
     fetchJobs();
   }, []);
-  // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
+const JobPage = () => {
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        console.log(data);
+        setJob(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, []);
+  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+};
+
+export default JobPage;

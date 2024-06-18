e81881a3e31b5350490236925d30b745a1b00d45
Sachin KM <hiscan99@gmail.com>
2024-06-18 18:13:53 +0200

    add job using forms

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
+import AddJobPage from "./pages/AddJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} />
+      <Route path="/add-job" element={<AddJobPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )

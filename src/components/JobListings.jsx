3136636cc770df1bda16d1ee36feefe827f97bd8
Sachin KM <hiscan99@gmail.com>
2024-06-18 12:26:09 +0200

    use json-server as backend

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-import jobs from "../jobs.json";
 import JobListing from "./JobListing";
+
 const JobListings = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:3005/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
+  // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -11,10 +28,15 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? "Recent Jobs" : "All Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {/* <!-- Job Listing 1 --> */}
-          {jobListings.map((job) => (
-            <JobListing job={job} key={job.id} />
-          ))}
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : (
+            <>
+              {jobs.map((job) => (
+                <JobListing job={job} key={job.id} />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>

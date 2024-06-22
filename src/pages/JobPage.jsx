0633999cfe9f0106f02339c1812a2f7637ba82c6
Sachin KM <hiscan99@gmail.com>
2024-06-22 18:45:46 +0200

    add react toastify

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,6 +2,7 @@ import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const JobPage = ({ deleteJob }) => {
   const navigate = useNavigate();
@@ -14,6 +15,7 @@ const JobPage = ({ deleteJob }) => {
     if (!confirm) return;
 
     deleteJob(jobId);
+    toast.success("Job deleted successfully!");
     navigate("/jobs");
   };
 
